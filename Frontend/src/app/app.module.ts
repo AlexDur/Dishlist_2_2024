ea1c265cf48ed98cbf3bb1b6e48c4a0edccb5f2e
Alusch <alexdurach@gmail.com>
2023-09-04 00:46:13 +0200

    [RA24]: Implementierung der Tabelle begonnen

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {TableModule} from "primeng/table";
+import { ListenContainerComponent } from './listen-container/listen-container.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListenContainerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TableModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
