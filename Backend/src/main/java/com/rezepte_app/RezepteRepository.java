1cf1ade8d45c6813452634c084b89ae08cc9e3fb
Alusch <alexdurach@gmail.com>
2024-03-25 12:24:54 +0100

    Sortierung nach ID der Rezepte

--- Backend/src/main/java/com/rezepte_app/RezepteRepository.java
+++ Backend/src/main/java/com/rezepte_app/RezepteRepository.java
@@ -3,9 +3,11 @@ package com.rezepte_app;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface RezepteRepository extends JpaRepository<Rezept, Integer> {
 
-    Rezept findById(int id);
+    List<Rezept> findAllByOrderByIdDesc();
 }
