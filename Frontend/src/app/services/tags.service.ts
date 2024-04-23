587d88814e8687e37a04b9f2d66f4108aaf3d07d
Alusch <alexdurach@gmail.com>
2024-04-24 01:24:03 +0200

    Aktive Tag in Post enthalten

--- Frontend/src/app/services/tags.service.ts
+++ Frontend/src/app/services/tags.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Tag } from '../models/tag';
+import {Dish} from "../tags/tags.component";
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,24 @@ export class TagService {
   private tagsSubject = new BehaviorSubject<Tag[]>([]);
   public tags$ = this.tagsSubject.asObservable();
 
+  updateActiveTags(activeTags: Set<Dish>): void {
+    const tags = this.tagsSubject.getValue(); // Aktuelle Tags abrufen
+    const updatedTags: Tag[] = [];
 
-  updateTags(tags: Tag[]): void {
-    this.tagsSubject.next(tags);
+    // Überprüfen, ob die Tag bereits im aktuellen Set enthalten ist, und sie aktualisieren oder hinzufügen
+    activeTags.forEach(activeTag => {
+      const existingTagIndex = tags.findIndex(tag => tag.label === activeTag);
+      if (existingTagIndex !== -1) {
+        // Tag existiert bereits, aktualisieren
+        updatedTags.push({ ...tags[existingTagIndex] });
+      } else {
+        // Tag existiert nicht, hinzufügen
+        updatedTags.push({ label: activeTag, severity: 'info' });
+      }
+    });
+
+    // Neue Tags dem aktuellen Zustand hinzufügen
+    this.tagsSubject.next([...tags, ...updatedTags]);
   }
 
   getSelectedTags(): Tag[] {
