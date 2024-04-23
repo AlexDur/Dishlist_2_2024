412bbebce2731a48f028bc241d0782074d640d81
Alusch <alexdurach@gmail.com>
2024-04-23 18:48:09 +0200

    Zwischenstand ActiveTag-Implementierung: BE wohl fertig, Fokus nachfolgend auf FE

--- Frontend/src/app/tags/tags.component.ts
+++ Frontend/src/app/tags/tags.component.ts
@@ -13,22 +13,30 @@ type Dish = 'Vorspeise' | 'Hauptgang' | 'Nachtisch';
 
 export class TagsComponent {
   @Input()currentRecipe: Rezept | undefined;
+
   // Anfangs- und aktuelle Severity-Zustände
   initialSeverities: Record<Dish, string> = {
-    Vorspeise: 'warning',  // Gelb
+    Vorspeise: 'warning',  // Orange
     Hauptgang: 'success',  // Grün
     Nachtisch: 'danger'    // Rot
   };
   currentSeverities: Record<Dish, string> = {...this.initialSeverities};
+  activeTags: Set<Dish> = new Set();
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
 
   handleClick(dish: Dish): void {
-    // Überprüfen, ob die aktuelle Severity 'info' ist
-    if (this.currentSeverities[dish] === 'info') {
-      // Zurücksetzen auf die ursprüngliche Severity
+    if (this.activeTags.has(dish)) {
+      // Deaktiviert das Tag
+      this.activeTags.delete(dish);
       this.currentSeverities[dish] = this.initialSeverities[dish];
     } else {
-      // Ändern der Severity auf 'info'
+      // Aktiviert das Tag
+      this.activeTags.add(dish);
+      console.log('dish', dish)
       this.currentSeverities[dish] = 'info';
     }
+    this.cdr.detectChanges();
   }
 }
