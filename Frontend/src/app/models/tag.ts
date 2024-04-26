67a982cf8a07054f1273897b0049c40e31854a0d
Alusch <alexdurach@gmail.com>
2024-04-26 16:03:25 +0200

    Tags werden korrekt bei POST mitgegeben, aber bei GET noch nicht

--- Frontend/src/app/models/tag.ts
+++ Frontend/src/app/models/tag.ts
@@ -1,5 +1,5 @@
 export interface Tag {
   id?:number;
   label: string;
-  severity: 'success' | 'info' | 'warning' | 'danger';
+  severity: 'success' | 'info' | 'warning' | 'danger' | 'default';
 }
--- Frontend/src/app/services/tags.service.ts
+++ Frontend/src/app/services/tags.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Tag } from '../models/tag';
-import {Dish} from "../tags/tags.component";
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,25 +10,26 @@ export class TagService {
   private tagsSubject = new BehaviorSubject<Tag[]>([]);
   public tags$ = this.tagsSubject.asObservable();
 
-  updateActiveTags(activeTags: Set<Dish>): void {
+  updateSelectedTags(selectedTags: Tag[]): void {
     const tags = this.tagsSubject.getValue(); // Aktuelle Tags abrufen
     const updatedTags: Tag[] = [];
 
-    // Überprüfen, ob die Tag bereits im aktuellen Set enthalten ist, und sie aktualisieren oder hinzufügen
-    activeTags.forEach(activeTag => {
-      const existingTagIndex = tags.findIndex(tag => tag.label === activeTag);
+    // Überprüfen, ob die Tags bereits im aktuellen Set enthalten sind, und sie aktualisieren oder hinzufügen
+    selectedTags.forEach(selectedTag => {
+      const existingTagIndex = tags.findIndex(tag => tag.label === selectedTag.label); // Korrektur: Vergleich der Labels
       if (existingTagIndex !== -1) {
         // Tag existiert bereits, aktualisieren
-        updatedTags.push({ ...tags[existingTagIndex] });
+        updatedTags.push({ ...tags[existingTagIndex], ...selectedTag }); // Annahme: selectedTag könnte aktualisierte Informationen haben
       } else {
         // Tag existiert nicht, hinzufügen
-        updatedTags.push({ label: activeTag, severity: 'info' });
+        updatedTags.push(selectedTag); // Annahme: selectedTag ist bereits vollständig definiert
       }
     });
 
-    // Neue Tags dem aktuellen Zustand hinzufügen
-    this.tagsSubject.next([...tags, ...updatedTags]);
+    // Aktualisieren des Subjects mit den neuen Tags
+    this.tagsSubject.next(updatedTags);
   }
+
 
   getSelectedTags(): Tag[] {
     return this.tagsSubject.getValue();
