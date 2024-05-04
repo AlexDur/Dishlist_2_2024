c8199cc7a0467733b34d350f9750a70ffc3d98e1
Alusch <alexdurach@gmail.com>
2024-05-04 02:19:20 +0200

    Annäherung Funktion, dass auf "Info" geschaltene Tags nach Neuladen als solche farblich weiterhin zu erkennen sind; Beginn mit Verfolgung des Ansatzes, dass außerhalb des Edit-Modus, nicht aktivierte Tags nicht sichtbar sind

--- Frontend/src/app/services/tags.service.ts
+++ Frontend/src/app/services/tags.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import {BehaviorSubject, forkJoin, Observable, throwError} from 'rxjs';
 import { Tag } from '../models/tag';
+import {HttpClient} from "@angular/common/http";
 
 
 @Injectable({
@@ -10,24 +11,37 @@ export class TagService {
   private tagsSubject = new BehaviorSubject<Tag[]>([]);
   public tags$ = this.tagsSubject.asObservable();
 
-  updateSelectedTags(selectedTags: Tag[]): void {
-    const tags = this.tagsSubject.getValue();
-    const updatedTags = tags.map(tag => {
-      const foundTag = selectedTags.find(t => t.label === tag.label);
-      return foundTag ? { ...tag, ...foundTag } : tag;
-    });
+  private backendUrl = 'http://localhost:8080';
 
-    // Fügen Sie fehlende Tags hinzu, die noch nicht in der Liste sind
-    selectedTags.forEach(tag => {
-      if (!updatedTags.some(t => t.label === tag.label)) {
-        updatedTags.push(tag);
-      }
-    });
+  constructor(private http: HttpClient) {}
 
-    this.tagsSubject.next(updatedTags);
+  updateTag(tag: Tag) {
+    if (!tag.id) {
+      console.error('Versuch, ein Tag ohne ID zu aktualisieren:', tag);
+      return throwError(() => new Error('Tag hat keine ID'));
+    }
+    return this.http.put<Tag>(`${this.backendUrl}/${tag.id}`, tag);
   }
 
 
+  updateSelectedTags(selectedTags: Tag[]): void {
+    const validTags = selectedTags.filter(tag => tag.id !== undefined);
+
+    if (validTags.length === 0) {
+      console.error('Keine gültigen Tags zum Aktualisieren gefunden.');
+      return;
+    }
+
+    forkJoin(validTags.map(tag => this.updateTag(tag))).subscribe({
+      next: (updatedTags) => {
+        console.log('Alle Tags wurden erfolgreich aktualisiert', updatedTags);
+        this.tagsSubject.next(updatedTags);
+      },
+      error: (error) => {
+        console.error('Fehler beim Aktualisieren der Tags', error);
+      }
+    });
+  }
 
   getSelectedTags(): Tag[] {
     return this.tagsSubject.getValue();
