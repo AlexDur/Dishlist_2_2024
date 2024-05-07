0133a7a96295759ae9a4efc6c162ebe0bf72b8b5
Alusch <alexdurach@gmail.com>
2024-05-07 12:52:31 +0200

    Splash- und Info_Screen angelegt

---
+++ Frontend/src/app/intro/info-screen/info-screen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-screen',
+  templateUrl: './info-screen.component.html',
+  styleUrls: ['./info-screen.component.scss']
+})
+export class InfoScreenComponent {
+
+}
