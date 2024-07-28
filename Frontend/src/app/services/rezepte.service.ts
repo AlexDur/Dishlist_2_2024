5fd99ccb95fa038523da1538089efc15af1f534d
Alusch <alexdurach@gmail.com>
2024-07-29 01:56:48 +0200

    ok, bei hinzu von mehreren Tags wird nur der Zähler des ersten der ausgewählten Tags erhöht
    Allerdings:
    - Filtern funkt für Gerichtart und Mahlzeit
    - Zählung erstmal gesetzter Tags funkt auch nach Neuladen

--- Frontend/src/app/services/rezepte.service.ts
+++ Frontend/src/app/services/rezepte.service.ts
@@ -1,9 +1,10 @@
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
-import { EventEmitter, Injectable } from '@angular/core';
-import { BehaviorSubject, catchError, tap, throwError, Observable } from 'rxjs';
+import {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
+import {EventEmitter, Injectable} from '@angular/core';
+import {BehaviorSubject, catchError, finalize, map, Observable, tap, throwError} from 'rxjs';
 import { Rezept } from '../models/rezepte';
-import { environment } from "../../environments/environment";
-import { Tag } from "../models/tag";
+import {config} from "../../environments/config";
+import {environment} from "../../environments/environment";
+import {Tag} from "../models/tag";
 
 interface RezeptAntwort {
   id: number;
@@ -17,25 +18,28 @@ export class RezeptService {
   public onRezeptUpdated: EventEmitter<void> = new EventEmitter();
   private backendUrl = environment.apiUrl;
 
+  // BehaviorSubject speichert den zuletzt gesendeten Wert und gibt diesen Wert sofort an neue Abonnenten weiter.
+  // Speichert Arrays vom Type Rezept
+  // Initialisiert das BehaviorSubject mit leerem Array
   private rezepteSubject: BehaviorSubject<Rezept[]> = new BehaviorSubject<Rezept[]>([]);
   public rezepte$: Observable<Rezept[]> = this.rezepteSubject.asObservable();
-  public kategorieZaehlerSubject: BehaviorSubject<{ [kategorie: string]: number }> = new BehaviorSubject({});
+  public kategorieZaehlerSubject: BehaviorSubject<{[kategorie: string]: number}> = new BehaviorSubject({});
   private loadingSubject = new BehaviorSubject<boolean>(false);
 
   private getJsonHeaders() {
     return new HttpHeaders({ 'Content-Type': 'application/json' });
   }
-
   constructor(private http: HttpClient) { }
 
   getAlleRezepte(): Observable<Rezept[]> {
     return this.http.get<Rezept[]>(`${this.backendUrl}/api/rezepte/alleRezepte`).pipe(
       tap(rezepte => {
+        // Verarbeiten der Rezepte und Umwandeln der Datumsstrings in Date-Objekte
         const processedRezepte = rezepte.map(rezept => ({
           ...rezept,
           datum: rezept.datum ? new Date(rezept.datum) : undefined
         }));
-        this.rezepteSubject.next(processedRezepte);
+        this.rezepteSubject.next(processedRezepte); // Aktualisieren des Subjects mit den neuen Daten
       }),
       catchError(error => {
         console.error("Fehler beim Laden der Rezepte", error);
@@ -44,6 +48,11 @@ export class RezeptService {
     );
   }
 
+  // Methode, die Rezept-Objekt akzeptiert und ein Observable zurückgibt, das eine HTTP-Antwort enthält
+  // LoadingSubject, um Ladezustand zu aktualisieren und um e. UI-Komponente darüber zu benachrichtigen (Subject = Observable UND Observer)
+  // JSON-Header (enthält Metadaten, s. oben) werden für die HTTP-Anfrage abgerufen, um sicherzustellen, dass die Anfrage als JSON formatiert ist.
+  // HTTP-POST-Anfrage wird an Server gesendet, um das Rezept zu erstellen (Rezept-Objekt + vorbereitete Header)
+  // tap (von "Anzapfen" eines Stroms [Observable]) zur Verarbeitung der Antwort, also zum Ausführen von Aktion beim Durchfluss der Daten
   createRezept(rezept: Rezept): Observable<HttpResponse<RezeptAntwort>> {
     console.log("Rezept vor dem Senden:", rezept);
 
@@ -51,8 +60,10 @@ export class RezeptService {
     const headers = this.getJsonHeaders();
     return this.http.post<RezeptAntwort>(`${this.backendUrl}/api/rezepte/create`, rezept, { headers, observe: 'response' }).pipe(
       tap(response => {
+        console.log('Server Response:', response); // Debugging-Meldung hinzufügen
         if (response.body) {
-          const updatedRezepte = [...this.rezepteSubject.getValue(), { ...rezept, id: response.body.id }];
+          console.log('Response Body:', response.body); // Debugging-Meldung hinzufügen
+          const updatedRezepte = [...this.rezepteSubject.getValue(), {...rezept, id: response.body.id}];
           this.rezepteSubject.next(updatedRezepte);
           this.updateKategorieZaehler(rezept.tags);
           this.onRezeptUpdated.emit();
@@ -61,18 +72,20 @@ export class RezeptService {
         }
       }),
       catchError(error => {
-        console.error('Unerwartete Antwort vom Server:', error);
+        console.error('Unerwartete Antwort vom Server:', error); // Ändere von Response zu error
         return throwError(() => error);
       })
     );
   }
 
+
   private updateKategorieZaehler(tags: Tag[] | undefined): void {
     const aktuelleZaehler = this.kategorieZaehlerSubject.getValue();
-    const aktualisierteZaehler = { ...aktuelleZaehler };
+    const aktualisierteZaehler = {...aktuelleZaehler};
 
     if (tags) {
       tags.forEach(tag => {
+        // Sicherstellen, dass sowohl tag als auch tag.label definiert sind
         if (tag && tag.label) {
           const kategorieName = tag.label;
           aktualisierteZaehler[kategorieName] = (aktualisierteZaehler[kategorieName] || 0) + 1;
@@ -97,7 +110,7 @@ export class RezeptService {
         if (index !== -1) {
           const updatedRezepte = [
             ...existingRezepte.slice(0, index),
-            { ...existingRezepte[index], ...rezept },
+            {...existingRezepte[index], ...rezept},
             ...existingRezepte.slice(index + 1)
           ];
           this.rezepteSubject.next(updatedRezepte);
@@ -109,7 +122,6 @@ export class RezeptService {
       })
     );
   }
-
 
   deleteRezept(id: number): Observable<any> {
     const apiUrl = `${this.backendUrl}/api/rezepte/delete/${id}`;
@@ -125,20 +137,7 @@ export class RezeptService {
     );
   }
 
-  private updateKategorieZaehlerForAllRezepte(rezepte: Rezept[]): void {
-    const zaehler: { [key: string]: number } = {};
-    rezepte.forEach(rezept => {
-      rezept.tags?.forEach(tag => {
-        if (tag && tag.label) {
-          const kategorieName = tag.label;
-          zaehler[kategorieName] = (zaehler[kategorieName] || 0) + 1;
-        }
-      });
-    });
-    this.kategorieZaehlerSubject.next(zaehler);
-  }
-
-  private updateTagCountsAfterDeletion(id: number) {
+  updateTagCountsAfterDeletion(id: number) {
     let currentRezepte = this.rezepteSubject.getValue();
     let deletedRezept = currentRezepte.find(rezept => rezept.id === id);
 
