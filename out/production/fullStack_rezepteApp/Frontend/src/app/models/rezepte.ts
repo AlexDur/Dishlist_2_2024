52d3e5df53d0208f6852f9ae88bae25707dea4c7
Alusch <alexdurach@gmail.com>
2024-10-01 02:09:28 +0200

    Daten wiederherstellt bei Editieren des Rezepts

--- out/production/fullStack_rezepteApp/Frontend/src/app/models/rezepte.ts
+++ out/production/fullStack_rezepteApp/Frontend/src/app/models/rezepte.ts
@@ -1,9 +1,15 @@
+import {Tag} from "./tag";
+
 export interface Rezept {
-  id?: number,
-  name?: string,
-  onlineAdresse?: string,
-  datum?: Date | undefined,
-  person?: string,
-  status?: string,
-  rating?: number
+
+  id: number;
+  name?: string;
+  onlineAdresse?: string;
+  datum?: Date | undefined;
+  person?: string;
+  status?: boolean;
+  bewertung?: number;
+  tags?: Tag[];
+  showDeleteButton?: boolean;
 }
+
