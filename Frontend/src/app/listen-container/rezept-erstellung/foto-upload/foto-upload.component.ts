0f4c0ac4c43e6d52970b0c383af8b5588b22b063
Alusch <alexdurach@gmail.com>
2024-10-21 02:54:46 +0200

    ngOnInit war ungenutzt, entfernt

--- Frontend/src/app/listen-container/rezept-erstellung/foto-upload/foto-upload.component.ts
+++ Frontend/src/app/listen-container/rezept-erstellung/foto-upload/foto-upload.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { Rezept } from "../../../models/rezepte";
 
 @Component({
@@ -6,16 +6,12 @@ import { Rezept } from "../../../models/rezepte";
   templateUrl: './foto-upload.component.html',
   styleUrls: ['./foto-upload.component.scss']
 })
-export class FotoUploadComponent implements OnInit {
+export class FotoUploadComponent {
 
   @Output() imageUploaded = new EventEmitter<File>(); // Emitter für das hochgeladene Bild
   @Input() rezepte: Rezept[] = []; // Liste der Rezepte
-  bildUrl: string = ""; // Bild-URL für das Rezept
 
-  ngOnInit() {
-    // Überprüfen, ob ein Bild vorhanden ist und das Platzhalterbild festlegen
-    this.bildUrl = this.rezepte.length > 0 && this.rezepte[0].bildUrl ? this.rezepte[0].bildUrl : '/static/images/Platzhalter_keinUpload.jpg';
-  }
+
 
   onFileSelected(event: Event) {
     const fileInput = event.target as HTMLInputElement;
