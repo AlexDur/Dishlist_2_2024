1fafe584bb9819f7bc91f38394378f0bbed447b3
Alusch <alexdurach@gmail.com>
2024-10-21 02:55:23 +0200

    rezeptAntwort im FE erstellt

---
+++ Frontend/src/app/models/rezeptAntwort.ts
@@ -0,0 +1,10 @@
+// src/app/models/rezeptAntwort.model.ts
+import {Tag} from "./tag";
+
+export interface RezeptAntwort {
+  id: number;
+  name: string;
+  onlineAdresse?: string;
+  tags?: Tag[];
+  bildUrl?: string;
+}
