9cd6970dfc1a1fb6ddbfd296fd681789c04458e0
Alusch <alexdurach@gmail.com>
2024-11-08 14:49:49 +0100

    Codestyle-Konfig für FE und BE

--- Frontend/src/app/models/default_tag.ts.ts
+++ Frontend/src/app/models/default_tag.ts.ts
@@ -11,7 +11,7 @@ export const DEFAULT_TAGS: Tag[] = [
   { label: 'Italienisch', count: 0, selected: false, type: 'Küche' },
   { label: 'Japanisch', count: 0, selected: false, type: 'Küche' },
   { label: 'Koreanisch', count: 0, selected: false, type: 'Küche' },
-  { label: 'Italienisch', count: 0, selected: false, type: 'Küche' },
+  { label: 'Mexikanisch', count: 0, selected: false, type: 'Küche' },
   { label: 'ballaststoffreich', count: 0, selected: false, type: 'Nährwert' },
   { label: 'kalorienarm', count: 0, selected: false, type: 'Nährwert' },
   { label: 'kalorienreich', count: 0, selected: false, type: 'Nährwert' },
