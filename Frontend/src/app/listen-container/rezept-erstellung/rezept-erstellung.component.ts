74bd7178d6be035f46c4bc50f7b89c9069a73cf5
Alusch <alexdurach@gmail.com>
2024-12-09 16:09:46 +0100

    Navi von RErstellung gefixt

--- Frontend/src/app/listen-container/rezept-erstellung/rezept-erstellung.component.ts
+++ Frontend/src/app/listen-container/rezept-erstellung/rezept-erstellung.component.ts
@@ -218,7 +218,7 @@ export class RezeptErstellungComponent implements OnInit {
 
   navigateContainer(event: Event) {
     event.preventDefault();
-    this.router.navigate(['/listencontainer']);
+    this.router.navigate(['/listen-container']);
   }
 
   getGerichtartenTags(): Tag[] {
