af86fb9b90ac18e088f703e65983bf1fafdfaa9b
Alusch <alexdurach@gmail.com>
2025-02-11 23:07:06 +0100

    gitignore Anpassungen

---
+++ Frontend/src/app/utils/spoonDataMapping.ts
@@ -0,0 +1,38 @@
+export const spoonDataMapping: { [category: string]: { [key: string]: string } } = {
+  dishTypes: {
+    starter: 'Vorspeise',
+    antipasti: 'Vorspeise',
+    antipasto: 'Vorspeise',
+    snack: 'Vorspeise',
+    condiment: 'Vorspeise',
+    dip: 'Vorspeise',
+    sauce: 'Vorspeise',
+    spread: 'Vorspeise',
+    'hor d\'oezvre': 'Vorspeise',
+    soup: 'Hauptgang',
+    lunch: 'Hauptgang',
+    Haupt: 'Hauptgang',
+    'main course': 'Hauptgang',
+    'main dish': 'Hauptgang',
+    diner: 'Hauptgang',
+    dessert: 'Nachtisch',
+    'Frühstück': 'Nachtisch',
+    breakfast: 'Nachtisch',
+    morning: 'Nachtisch',
+  },
+  cuisines: {
+    Chinese: 'Chinesisch',
+    German: 'Deutsch',
+    Indian: 'Indisch',
+    Italian: 'Italienisch',
+    Japanese: 'Japanisch',
+    Korean: 'Koreanisch',
+    Mexican: 'Mexikanisch',
+  },
+  /*  'ballaststoffreich'
+  'kalorienarm'
+  'kalorienreich'
+  'proteinreich'
+  'zuckerarm'*/
+
+};
