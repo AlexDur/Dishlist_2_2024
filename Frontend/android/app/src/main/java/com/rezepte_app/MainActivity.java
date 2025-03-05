2d60bedb53f4d85047c14713e6671cb3f23c49a4
Alusch <alexdurach@gmail.com>
2025-03-05 20:29:09 +0100

    KA

---
+++ Frontend/android/app/src/main/java/com/rezepte_app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rezepte_app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+
+/*  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    com.rezepte_app.BillingHelper billingHelper = new de.dishlist.app.BillingHelper();
+    billingHelper.initializeBillingClient(this);
+  }*/
+
+}
