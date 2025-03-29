a3172e84387bc1667516e05557e2e67099a8e11a
Alusch <alexdurach@gmail.com>
2025-03-29 22:49:53 +0100

    Bundle dadurch baubar

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.dishlist_app',
-  appName: 'DishList',
-  webDir: 'www.dish-list.de'
-};
-
-export default config;
